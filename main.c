412956fe6e5fba5154d0fdfdd9da58de7a8d8fd3
Herschenglime <herschenglime@gmail.com>
2025-10-01 17:22:57 -0400

    added some very simple sample code to make sure building and debugging works

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// TIVA include to make sure it's linking properly
 #include <driverlib/gpio.h>
 
 /**
@@ -9,5 +10,9 @@
  */
 int main(void)
 {
+    int i = 0;
+    while(true) {
+        i++;
+    }
 	return 0;
 }
